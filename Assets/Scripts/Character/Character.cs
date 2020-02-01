8e920b2a9b19652b40db41d89fb5957516b81c80
Aleksander Andresen <aleksander.andresen@nav.no>
2020-02-01 19:54:55 +0100

    konf racoon

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -7,12 +7,19 @@ using System.IO;
 public class Character : MonoBehaviour
 {
     public TextAsset config;
-    public int id;
+    public Behaviour behaviour;
 
 
-    public void Awake()
+    public void Start()
     {
-       
+        var behaviourSerializer = new XmlSerializer(typeof(Behaviour));
+
+        using (TextReader reader = new StringReader(config.ToString()))
+        {
+            behaviour = (Behaviour)behaviourSerializer.Deserialize(reader);
+
+        }
+        int x = 5;
     }
 
     public void Update()
